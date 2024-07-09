2457fd50fd8eecdb11e8b1dee74ef019fcbe7d7a
Abdykali Azat <azat.abdykalli@gmail.com>
2024-07-09 13:18:17 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://miras-cargo-astana.kz:3001',
+    
+    nameCargo: 'Miras-Cargo-Astana'
+  };
+  
+  
+export default config;
+  
